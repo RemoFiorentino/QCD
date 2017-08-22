36e3a7eb65b43d20028e2d64ee18de52098d4a8e
Remo Fiorentino <rfiorentino@uninorte.edu.co>
2017-08-22 15:42:56 +0000

    Eliminando presence de numero de estudiantes de asignaturas

--- app/models/asignatura.rb
+++ app/models/asignatura.rb
@@ -3,7 +3,7 @@ class Asignatura < ActiveRecord::Base
   has_many :qcds, dependent: :destroy
   validates :nombre, presence: true, length: { minimum: 3, maximum: 40 }
   validates :nrc, presence: true, length: { minimum: 4, maximum: 4}
-  validates :division,:dpto,:programa,:semestre,:creditos,:objetivo1,:num_estud, presence: true
+  validates :division,:dpto,:programa,:semestre,:creditos,:objetivo1, presence: true
   def self.search(nrc, semestre)
     where("nrc LIKE #{nrc}", "semestre LIKE #{semestre}")
   end
