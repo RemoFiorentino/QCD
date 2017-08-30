7fa938c7a6c61083b2a9b8de0178e4e3132e6bcd
Remo Fiorentino <rfiorentino@uninorte.edu.co>
2017-08-30 20:13:45 +0000

    rack cors

--- config/application.rb
+++ config/application.rb
@@ -29,11 +29,5 @@ module Workspace
     end
     require 'pdfkit'
     config.middleware.use PDFKit::Middleware
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
   end
 end
