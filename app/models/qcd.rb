6347b501cb3c62a098c361105f51bafddccc2edf
Remo Fiorentino <rfiorentino@uninorte.edu.co>
2017-08-30 23:05:11 +0000

    mail add

--- app/models/qcd.rb
+++ app/models/qcd.rb
@@ -7,10 +7,10 @@ class Qcd < ActiveRecord::Base
     link_to informe,  asignatura_qcd_path(qcd.id)
   end
   
-  # after_update :send_mail
-  # def send_mail
-  #   if( self.estados != 0)
-  #     Mailer.profe_email(self.asignatura.user,self).deliver
-  #   end
-  # end
+  after_update :send_mail
+  def send_mail
+    if( self.estados != 0)
+      Mailer.profe_email(self.asignatura.user,self).deliver
+    end
+  end
 end
